d3cc5cb0f2bd1ed5b82e581d5d22cb0487a6c5cc
Alessandro B Moreira <alebm56@gmail.com>
2020-10-08 02:12:47 +0000

    Transformando o server em módulo ESM para o nodeJS.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import http from "http";
 const app = express();
 const server = http.createServer(app);
 
-app.use.express.static("public");
+app.use(express.static("public"));
 
 server.listen(3000, () => {
   console.log(`Server listening on port: 3000`);
